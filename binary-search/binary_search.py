ee5a9ef3525d567da560a3d5706e17bb89047249
Arman <32128949+AubakirovArman@users.noreply.github.com>
2022-04-08 13:14:00 +0600

    answer 2

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,4 +1,17 @@
 def binary_search(lst, to_find):
+    if to_find in lst:
+        if len(lst)==1:
+            return 0
+        else:
+            mid=int(len(lst)/2)
+            if lst[mid]>to_find:
+                return binary_search(lst[:mid], to_find)
+            elif lst[mid]<to_find:
+                return binary_search(lst[mid:], to_find)+mid
+            else:
+                return mid
+    else:
+        return -1
     # search for the element to_find inside lst
     # if found, return index of element
     # else return -1

e2c9fb095abcd764f4ae7a01fe3e12d81514a0c0
Arman <32128949+AubakirovArman@users.noreply.github.com>
2022-06-22 20:21:28 +0600

    update

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,19 +1,31 @@
-def binary_search(array, element):
-    mid = 0
-    start = 0
-    end = len(array)
-    step = 0
 
-    while (start <= end):
-        
-        step = step+1
-        mid = (start + end) // 2
-
-        if element == array[mid]:
-            return mid
-
-        if element < array[mid]:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return -1
+def binary_search(lst, fnd):
+    if lst[0]==fnd:
+        return 0
+    elif lst[len(lst)-1]==fnd:
+        return len(lst)-1
+    elif len(lst)==2:
+        return -1
+    elif len(lst)==1 and lst[0]!=fnd:
+        return -1
+    col=0
+    mid=len(lst)//2
+    # print('*')
+    # print(mid)
+    # print(len(lst[:mid]))
+    # print(len(lst[mid:]))
+    # print(lst[:mid])
+    # print(lst[mid:])
+    # print(lst)
+    # print('*')
+    # input()
+    if lst[mid]>fnd and (lst[mid-1]<fnd) ==False:
+        return binary_search(lst[:mid], fnd)
+    elif lst[mid]<fnd and (lst[mid+1]>fnd) == False:
+        col=len(lst[:mid])
+        return binary_search(lst[mid:], fnd)+col
+    elif lst[mid]==fnd:
+        return mid+col
+    elif lst[mid]!=fnd and lst[mid+1]>fnd and lst[mid-1]<fnd:
+        return -1
+    

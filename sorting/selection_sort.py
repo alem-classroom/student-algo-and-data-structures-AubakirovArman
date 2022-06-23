8d72e72bad8d5ce7e4bbff4fa26111bb1689979c
Arman <32128949+AubakirovArman@users.noreply.github.com>
2022-06-23 09:05:39 +0600

    ы

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,2 +1,8 @@
 def selection_sort(lst):
-    # sort the list via selection sort
+    for bs in range(len(lst)):
+        for i in range(len(lst)-1):
+            if lst[i]>lst[i+1]:
+                b=lst[i+1]
+                lst[i+1]=lst[i]
+                lst[i]=b
+    return lst

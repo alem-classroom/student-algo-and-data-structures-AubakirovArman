196984bee9f3b55c48a3f264fcfca88575e7b41a
Arman <32128949+AubakirovArman@users.noreply.github.com>
2022-06-23 09:48:56 +0600

    s

--- dynamic-programming-1/steps.py
+++ dynamic-programming-1/steps.py
@@ -1,3 +1,13 @@
 def steps(num):
-    # solve the problem via dynamic programming
-    # return number of required steps
+    if num==0:
+        return 0
+    elif num==1:
+        return 1
+    fac=0
+    fac2=1
+    for i in range(num):
+        d=fac+fac2
+        fac=fac2
+        fac2=d
+    return fac
+

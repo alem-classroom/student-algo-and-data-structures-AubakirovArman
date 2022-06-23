78569f0c0b3bbd19df7633fcc02972d53280d086
Arman <32128949+AubakirovArman@users.noreply.github.com>
2022-06-23 09:02:10 +0600

    update

--- bubble-sort/bubble_sort.py
+++ bubble-sort/bubble_sort.py
@@ -1,2 +1,9 @@
 def bubble_sort(lst):
+    for bs in range(len(lst)):
+        for i in range(len(lst)-1):
+            if lst[i]>lst[i+1]:
+                b=lst[i+1]
+                lst[i+1]=lst[i]
+                lst[i]=b
+    return lst
     # sort the list via bubble sort
